aa356fdab297c4160bdc2fe87f8bede714033ac2
Douglas Uretsky <douglasuretsky@gmail.com>
2018-10-22 14:29:25 -0400

    first commit

---
+++ lib/celtics_roster.rb
@@ -0,0 +1,5 @@
+require "celtics_roster/version"
+
+module CelticsRoster
+  # Your code goes here...
+end
---
+++ spec/celtics_roster_spec.rb
@@ -0,0 +1,9 @@
+RSpec.describe CelticsRoster do
+  it "has a version number" do
+    expect(CelticsRoster::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
