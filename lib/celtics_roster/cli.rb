7aa8cf99632cc898c5804db93d807557600f0e77
Douglas Uretsky <douglasuretsky@gmail.com>
2018-10-24 15:45:26 -0400

    finish get_players

--- lib/celtics_roster/cli.rb
+++ lib/celtics_roster/cli.rb
@@ -3,48 +3,58 @@ require 'pry'
 class CelticsRoster::CLI
 
   def call
-    list_players
-    see_player_bio
+    display_roster
+    menu
+    goodbye
   end
 
-  def list_players
-    #get_player
-    puts  <<-DOC.gsub /^\s/, ""
-    "Your Boston Celtics"
-      1. player one
-      2. player two
-      3. player three
-      4. player four
-    DOC
+  def display_roster
+    CelticsRoster::Player.generate_players
+    CelticsRoster::Player.display_roster
   end
 
-  def see_player_bio
-    puts "select a player to see their bio"
+  # def menu
+  #   puts "Select the player's 'list number' to see their more info., or enter 'exit'"
+  #   input = gets.chomp
+  #   until input == 'exit'
+  #     if valid_input?(input)
+  #       player = CelticsRoster::Player.select_player(input.to_i)
+  #       puts player.name
+  #       menu
+  #     else
+  #       puts "Invalid input. Please re-enter"
+  #       menu
+  #     end
+  #   end
+  # end
+
+  def menu
+    input = get_user_input
+    if valid_input?(input)
+      player = CelticsRoster::Player.select_player(input.to_i)
+      puts player.name
+      menu
+    elsif input == 'exit'
+      return
+    else
+      puts "Invalid input. Please re-enter."
+      menu
+    end
+  end
+
+private
+
+  def get_user_input
+    puts "Select the player's 'list number' to see their more info., or enter 'exit'"
     input = gets.chomp
-    unless exit(input)
-      case input
-      when "1"
-        puts "player one"
-      when "2"
-        puts "player two"
-      when "3"
-        puts "player three"
-      when "4"
-        puts "player 4"
-      else
-        puts "invalid entry"
-        sleep(2)
-        system "clear"
-        call
-      end
-    end
   end
 
-  def exit(input)
-    if input == "exit"
-      return true
-    end
-    return false
+  def valid_input?(input)
+    return true if input.to_i.between?(1, CelticsRoster::Player.all.count)
+  end
+
+  def goodbye
+    puts "Red Quote"
   end
 
 end
--- lib/celtics_roster/player.rb
+++ lib/celtics_roster/player.rb
@@ -1,23 +1,33 @@
-require 'nokogiri'
-require 'open-uri'
-
 class CelticsRoster::Player
   attr_accessor :name, :number, :position
 
   @@full_roster = []
 
-  def initialize(name, number, position)
+  def initialize(name, position, number, url)
     @name = name
     @number = number
     @position = position
+    @url = url
+
+    @@full_roster << self
   end
 
-  def self.create_player(url)
-    html = open(url)
-    doc = Nokogiri::HTML(html)
+  def self.generate_players
+    Scraper.get_players
   end
 
   def self.all
     @@full_roster
+  end
+
+  def self.display_roster
+    self.all.each_with_index do |player, index|
+      puts "#{index + 1} - #{player.name} - #{player.position}"
+    end
+    nil
+  end
+
+  def self.select_player(input)
+    self.all[input - 1]
   end
 end
